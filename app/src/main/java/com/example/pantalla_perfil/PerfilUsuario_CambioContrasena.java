4fb3bc1eee000975846e03b698e90fa3581c901f
claudia.rodriguez <pazclaudia2019@gmail.com>
2023-10-26 17:52:11 -0600

    2 revision

---
+++ app/src/main/java/com/example/pantalla_perfil/PerfilUsuario_CambioContrasena.java
@@ -0,0 +1,14 @@
+package com.example.pantalla_perfil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PerfilUsuario_CambioContrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario_cambio_contrasena);
+    }
+}
---
+++ app/src/main/java/com/example/pantalla_perfil/Usuarioperfil_CambioContrasena.java
@@ -0,0 +1,14 @@
+package com.example.pantalla_perfil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Usuarioperfil_CambioContrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuarioperfil_cambio_contrasena);
+    }
+}
